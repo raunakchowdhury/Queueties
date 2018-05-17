32fb639413b4194128ed99ec564c43b1e436a8e3
Susan Lin <slin15@stuy.edu>
2018-05-17 15:48:14 -0400

    files added

---
+++ rmed/RunMed.java
@@ -0,0 +1,17 @@
+public class RunMed { 
+   private ALHeapMin lilVals; 
+   private ALHeapMax bigVals; 
+
+   public runMed (ALHeapMin a, ALHeapMax b){ 
+    lilVals = a; 
+    bigVals = b; 
+   }
+
+   public int getMedian() { 
+       return 0; 
+   }
+
+   public void add(Integer newVal) {
+       
+   }
+}
