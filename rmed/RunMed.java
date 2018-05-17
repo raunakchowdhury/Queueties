e9e8665d34afebefdc6f85c37cace38ed0bbe89b
raunakchowdhury <rchowdhury5@stuy.edu>
2018-05-17 17:36:14 -0400

    fixed bug causing the driver file to crash

--- rmed/RunMed.java
+++ rmed/RunMed.java
@@ -1,56 +1,58 @@
 import java.io.File;
 import java.util.Scanner;
 
-public class RunMed { 
-   private ALHeapMin lilVals; 
-   private ALHeapMax bigVals; 
+public class RunMed {
+private ALHeapMin lilVals;
+private ALHeapMax bigVals;
 
 /*****************************************************
 * default constructor  ---  inits empty heap
 *****************************************************/
-   public RunMed(){ 
-    lilVals = new ALHeapMin(); 
-    bigVals = new ALHeapMax(); 
-   }
+public RunMed(){
+        lilVals = new ALHeapMin();
+        bigVals = new ALHeapMax();
+}
 
 /*****************************************************
-* algo for getMedian 
+* algo for getMedian
 * if (input < rootMax)
-*   max.add(input); 
-* else 
-*   min.add(input); 
+*   max.add(input);
+* else
+*   min.add(input);
 * balanceHeaps(); (sizeDiff < 2)
 * if (sizeDiff >= 2)
-*   MinSize(max, min).add(MaxSize(max, min).remove()); 
+*   MinSize(max, min).add(MaxSize(max, min).remove());
 * if (maxSize == minSize)
-*   median = mean(maxRoot, minRoot); 
-* else 
-*   median = maxRoot; 
+*   median = mean(maxRoot, minRoot);
+* else
+*   median = maxRoot;
 *****************************************************/
-   public double getMedian() { 
-        double med; 
+public double getMedian() {
+        double med;
 
         if (bigVals.size() == lilVals.size())
-            med = (bigVals.peekMax() + lilVals.peekMin()) / 2.0; 
-        else 
-            med = bigVals.peekMax(); 
-        return med; 
-   }
+                med = (bigVals.peekMax() + lilVals.peekMin()) / 2.0;
+        else
+                med = bigVals.peekMax();
+        return med;
+}
 
-   public void add(Integer newVal) {
-        if (newVal < bigVals.peekMax())
-            bigVals.add(newVal); 
-        else 
-            lilVals.add(newVal); 
-        balance(); 
-   }
+public void add(Integer newVal) {
+        if (bigVals.size() == 0) //if this is the first int, just add it
+                bigVals.add(newVal);
+        else if (newVal < bigVals.peekMax())
+                bigVals.add(newVal);
+        else
+                lilVals.add(newVal);
+        balance();
+}
 
-   public void balance() { 
-       if (Math.abs(bigVals.size() - lilVals.size()) >= 2) {
-            if (lilVals.size() <= bigVals.size())
-                lilVals.add(bigVals.removeMax()); 
-            else 
-                bigVals.add(lilVals.removeMin()); 
-       }       
-   }
+public void balance() {
+        if (Math.abs(bigVals.size() - lilVals.size()) >= 2) {
+                if (lilVals.size() <= bigVals.size())
+                        lilVals.add(bigVals.removeMax());
+                else
+                        bigVals.add(lilVals.removeMin());
+        }
+}
 }
--- rmed/RunMedDriver.java
+++ rmed/RunMedDriver.java
@@ -19,36 +19,36 @@
     Clyde "Thluffy" Sinclair
     APCS2
     2018-05-17
-*/
+ */
 
 import java.util.Scanner;
 
 public class RunMedDriver {
 
-  public static void main( String[] args ) {
-    RunMed r = new RunMed();
+public static void main( String[] args ) {
+        RunMed r = new RunMed();
 
-    int n;
-    double median;
-    int count = 0;
-    Scanner sc = new Scanner( System.in );
+        int n;
+        double median;
+        int count = 0;
+        Scanner sc = new Scanner( System.in );
 
-    while( sc.hasNextInt() ) {
-      try {
-        n = sc.nextInt();
-        System.out.print("read " + n + "\n");
+        while( sc.hasNextInt() ) {
+                try {
+                        n = sc.nextInt();
+                        System.out.print("read " + n + "\n");
 
-        count++;
-        System.out.print("this many ints have been seen: " + count + "\n");
+                        count++;
+                        System.out.print("this many ints have been seen: " + count + "\n");
 
-        r.add(n);
-        median = r.getMedian();
-        System.out.print("median is now " + median + "\n");
-      } 
-      catch (Exception e) {
-        System.err.println("BOOP! probs w yer input:\n"+e);
-      }
-    }
+                        r.add(n);
+                        median = r.getMedian();
+                        System.out.print("median is now " + median + "\n");
+                }
+                catch (Exception e) {
+                        System.err.println("BOOP! probs w yer input:\n"+e);
+                }
+        }
 
-  }//end main
+}  //end main
 }//end class
